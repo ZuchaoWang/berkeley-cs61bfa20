4db3f4767f6933d5b8958e2ffc48853303dd857e
ZuchaoWang <zuchao.wang@gmail.com>
2022-06-14 18:52:44 +0800

    lab6

--- lab6/UnionFind.java
+++ lab6/UnionFind.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 public class UnionFind {
     int[] parent;
 
@@ -15,7 +13,9 @@ public class UnionFind {
 
     /* Throws an exception if v1 is not a valid vertex. */
     private void validate(int v1) {
-        // TODO
+        if (v1 < 0 || v1 >= parent.length) {
+            throw new IllegalArgumentException("Not a valid vertex!");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
@@ -27,13 +27,13 @@ public class UnionFind {
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
+        validate(v1);
         return parent[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean isConnected(int v1, int v2) {
-        // TODO
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -42,14 +42,40 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void connect(int v1, int v2) {
-        // TODO
+        int r1 = find(v1);
+        int r2 = find(v2);
+        if (r1 != r2) {
+            int s1 = -1 * parent[r1];
+            int s2 = -1 * parent[r2];
+            if (s1 > s2) {
+                parent[r1] = -1 * (s1 + s2);
+                parent[r2] = r1;
+            } else {
+                parent[r2] = -1 * (s1 + s2);
+                parent[r1] = r2;
+            }
+        }
     }
 
     /* Returns the root of the set v1 belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int v1) {
-        // TODO
-        return -1;
+        int cur = v1,
+            p = parent(cur);
+        while (p >= 0) {
+            cur = p;
+            p = parent(p);
+        }
+        int root = cur;
+        // path compression
+        cur = v1;
+        p = parent(cur);
+        while (p >= 0) {
+            parent[cur] = root;
+            cur = p;
+            p = parent(p);
+        }
+        return root;
     }
 
 }
