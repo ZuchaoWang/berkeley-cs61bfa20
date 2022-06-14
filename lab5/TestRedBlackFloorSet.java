02c2e2eef404fed4c5c96e5396c0185e83c82a11
ZuchaoWang <zuchao.wang@gmail.com>
2022-06-14 09:54:21 +0800

    lab5

--- lab5/TestRedBlackFloorSet.java
+++ lab5/TestRedBlackFloorSet.java
@@ -8,15 +8,19 @@ public class TestRedBlackFloorSet {
     @Test
     public void randomizedTest() {
         int N = 1000000;
-        double M = 5000;
         int L = 100000;
-        double eps = 0.000001;
+        double M = 5000;
+        double delta = 0.000001;
         AListFloorSet as = new AListFloorSet();
         RedBlackFloorSet rs = new RedBlackFloorSet();
         for(int i=0; i<N; i++) {
             double x = StdRandom.uniform(-M, M);
             as.add(x);
             rs.add(x);
+        }
+        for(int i=0; i<L; i++) {
+            double x = StdRandom.uniform(-M, M);
+            assertEquals(as.floor(x), rs.floor(x), delta);
         }
     }
 }
