1b9d5dbe5ccedf51884629c51cabe35603169785
zuchao.wang <zuchao.wang@gmail.com>
2022-06-22 01:15:37 +0800

    lab8 halfway

--- lab8/MyHashMap.java
+++ lab8/MyHashMap.java
@@ -1,14 +1,15 @@
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 public class MyHashMap<K, V> implements Map61B<K, V> {
-  private class MayHashMapNode {
+  private class MyHashMapNode {
     public K key;
     public V value;
-    public MayHashMapNode(K k, V v) {
+    public MyHashMapNode(K k, V v) {
       key = k;
       value = v;
     }
@@ -18,8 +19,9 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
   private final double defaultLoadfactor = 0.75;
   private final int resizeFactor = 2;
 
-  List<MayHashMapNode>[] hashTable;
+  List<MyHashMapNode>[] hashTable;
   double lf;
+  int n;
 
   public MyHashMap() {
     initMyHashMap(defaultInitialSize, defaultLoadfactor);
@@ -33,50 +35,108 @@ public class MyHashMap<K, V> implements Map61B<K, V> {
   }
 
   private void initMyHashMap(int initialSize, double loadFactor) {
-    hashTable = (List<MayHashMapNode>[]) new Object[initialSize];
+    hashTable = (List<MyHashMapNode>[]) new Object[initialSize];
     lf = loadFactor;
     clear();
   }
 
   @Override
   public Iterator<K> iterator() {
-    // TODO Auto-generated method stub
-    return null;
+    Set<K> s = keySet();
+    return s.iterator();
   }
 
   @Override
   public void clear() {
     Arrays.fill(hashTable, null);
+    n = 0;
   }
 
   @Override
   public boolean containsKey(K key) {
-    // TODO Auto-generated method stub
+    if (key == null) throw new IllegalArgumentException();
+    int index = Math.floorMod(key.hashCode(), hashTable.length);
+    List<MyHashMapNode> bucket = hashTable[index];
+    if (bucket != null) {
+      for (MyHashMapNode node: bucket) {
+        if(key == node.key) return true;
+      }
+    }
     return false;
   }
 
   @Override
   public V get(K key) {
-    // TODO Auto-generated method stub
+    if (key == null) throw new IllegalArgumentException();
+    int index = Math.floorMod(key.hashCode(), hashTable.length);
+    List<MyHashMapNode> bucket = hashTable[index];
+    if (bucket != null) {
+      for (MyHashMapNode node: bucket) {
+        if(key == node.key) return node.value;
+      }
+    }
     return null;
   }
 
   @Override
   public int size() {
-    // TODO Auto-generated method stub
-    return 0;
+    return n;
   }
 
   @Override
   public void put(K key, V value) {
-    // TODO Auto-generated method stub
-    
+    if (key == null) throw new IllegalArgumentException();
+    if (value == null) throw new IllegalArgumentException();
+    int index = Math.floorMod(key.hashCode(), hashTable.length);
+    List<MyHashMapNode> bucket = hashTable[index];
+    if (bucket == null) {
+      hashTable[index] = new LinkedList<MyHashMapNode>();
+      bucket = hashTable[index];
+    }
+    for (MyHashMapNode node: bucket) {
+      if(key == node.key) {
+        node.value = value;
+        return;
+      }
+    }
+    bucket.add(new MyHashMapNode(key, value));
+    n++;
+    if (n > hashTable.length * lf) {
+      resize(hashTable.length * resizeFactor);
+    }
+  }
+
+  private void resize(int newSize) {
+    List<MyHashMapNode>[] newHashTable = (List<MyHashMapNode>[]) new Object[newSize];
+    for(int i=0; i<hashTable.length; i++) {
+      List<MyHashMapNode> bucket = hashTable[i];
+      if (bucket != null) {
+        for (MyHashMapNode node: bucket) {
+          int newIndex = Math.floorMod(node.key.hashCode(), newSize);
+          List<MyHashMapNode> newBucket = newHashTable[newIndex];
+          if (newBucket == null) {
+            newHashTable[newIndex] = new LinkedList<MyHashMapNode>();
+            newBucket = newHashTable[newIndex];
+          }
+          newBucket.add(node);
+        }
+      }
+    }
+    hashTable = newHashTable;
   }
 
   @Override
   public Set<K> keySet() {
-    // TODO Auto-generated method stub
-    return null;
+    Set<K> s = new HashSet<>();
+    for(int i=0; i<hashTable.length; i++) {
+      List<MyHashMapNode> bucket = hashTable[i];
+      if (bucket != null) {
+        for (MyHashMapNode node: bucket) {
+          s.add(node.key);
+        }
+      }
+    }
+    return s;
   }
 
   @Override
