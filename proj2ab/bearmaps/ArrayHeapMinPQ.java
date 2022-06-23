b56c0c4ae5b5cea9da97fe0ae7d7262564b3f439
ZuchaoWang <zuchao.wang@gmail.com>
2022-06-23 12:38:02 +0800

    proj2b let's ignore efficiency test

--- proj2ab/bearmaps/ArrayHeapMinPQ.java
+++ proj2ab/bearmaps/ArrayHeapMinPQ.java
@@ -28,11 +28,15 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
   public void add(T item, double priority) {
     if (item == null || contains(item))
       throw new IllegalArgumentException();
-    int sz = size();
     ArrayHeapMinPQNode node = new ArrayHeapMinPQNode(item, priority);
-    items.add(sz+1, node);
-    itemIndex.put(item, sz+1);
-    siftUp(sz+1);
+    addNodeToTail(node);
+    siftUp(size());
+  }
+
+  private void addNodeToTail(ArrayHeapMinPQNode node) {
+    int sz = size();
+    items.add(sz + 1, node);
+    itemIndex.put(node.item, sz + 1);
   }
 
   @Override
@@ -45,22 +49,28 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
 
   @Override
   public T getSmallest() {
-    if (size() == 0) throw new NoSuchElementException();
+    if (size() == 0)
+      throw new NoSuchElementException();
     return items.get(1).item;
   }
 
   @Override
   public T removeSmallest() {
     int sz = size();
-    if (sz == 0) throw new NoSuchElementException();
+    if (sz == 0)
+      throw new NoSuchElementException();
     else {
-      ArrayHeapMinPQNode firstNode = items.get(1);
       swap(1, sz);
-      items.remove(sz);
-      itemIndex.remove(firstNode.item);
+      ArrayHeapMinPQNode node = removeNodeFromTail();
       siftDown(1);
-      return firstNode.item;
+      return node.item;
     }
+  }
+
+  private ArrayHeapMinPQNode removeNodeFromTail() {
+    ArrayHeapMinPQNode node = items.remove(size());
+    itemIndex.remove(node.item);
+    return node;
   }
 
   @Override
@@ -70,9 +80,16 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
 
   @Override
   public void changePriority(T item, double priority) {
-    if (!contains(item)) throw new NoSuchElementException();
-    // TODO Auto-generated method stub
-
+    if (!contains(item))
+      throw new NoSuchElementException();
+    int index = itemIndex.get(item);
+    ArrayHeapMinPQNode node = items.get(index);
+    double oldPriority = node.priority;
+    node.priority = priority;
+    if (priority > oldPriority)
+      siftDown(index);
+    else if (priority < oldPriority)
+      siftUp(index);
   }
 
   private void siftUp(int k) {
@@ -89,10 +106,10 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
 
   private void siftDown(int k) {
     int sz = size();
-    while(leftChild(k) <= sz) {
+    while (leftChild(k) <= sz) {
       int l = leftChild(k),
-        r = rightChild(k),
-        m = l; // min of l and r
+          r = rightChild(k),
+          m = l; // min of l and r
       if (r <= sz && items.get(r).priority < items.get(l).priority) {
         m = r;
       }
@@ -106,7 +123,8 @@ public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
   }
 
   private void swap(int k, int l) {
-    if (k == l) return;
+    if (k == l)
+      return;
     ArrayHeapMinPQNode oldKNode = items.get(k),
         oldLNode = items.get(l);
     items.set(k, oldLNode);
--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -3,5 +3,100 @@ package bearmaps;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ArrayHeapMinPQTest {
+  @Test
+  public void testSimpleCase() {
+    NaiveMinPQ<String> pq1 = new NaiveMinPQ<String>();
+    performTestSimpleCase(pq1);
+    ArrayHeapMinPQ<String> pq2 = new ArrayHeapMinPQ<String>();
+    performTestSimpleCase(pq2);
+  }
+
+  private void performTestSimpleCase(ExtrinsicMinPQ<String> pq) {
+    String[] strs = { "A", "B", "C" };
+    double[] prios = { 2.0, 1.0, 3.0 };
+    for (int i = 0; i < 3; i++) {
+      pq.add(strs[i], prios[i]);
+    }
+    assertEquals("B", pq.getSmallest());
+    assertEquals(3, pq.size());
+    assertEquals(true, pq.contains("B"));
+
+    pq.changePriority("B", 4.0);
+    assertEquals(3, pq.size());
+    assertEquals("A", pq.removeSmallest());
+    assertEquals(false, pq.contains("A"));
+    assertEquals(2, pq.size());
+    assertEquals("C", pq.removeSmallest());
+    assertEquals(1, pq.size());
+    assertEquals("B", pq.removeSmallest());
+    assertEquals(0, pq.size());
+  }
+
+  @Test
+  public void testRandomCase() {
+    List<String> curStrs = new ArrayList<String>();
+
+    int seed = 0;
+    int N = 10000;
+    double R = 10000;
+
+    Random rg = new Random(seed);
+    NaiveMinPQ<String> pq1 = new NaiveMinPQ<String>();
+    ArrayHeapMinPQ<String> pq2 = new ArrayHeapMinPQ<String>();
+
+    int op;
+    int rdInt;
+    double rdDb;
+    String curStr;
+
+    for (int i = 0; i < 2 * N; i++) {
+      if (i < N) { // first N rounds
+        if (curStrs.size() > 0) op = rg.nextInt(8); // if not empty, add has 2 chances
+        else op = 6; // if empty, must add
+      } else { // second N rounds
+        if (curStrs.size() > 0) op = rg.nextInt(6); // if not empty, add has no chance
+        else break; // if empty, end test
+      }
+      switch (op) {
+        case 0: // size
+          assertEquals(pq1.size(), pq2.size());
+          break;
+        case 1: // getSmallest
+          assertEquals(pq1.getSmallest(), pq2.getSmallest());
+          break;
+        case 2: // removeSmallest
+          curStrs.remove(pq1.getSmallest());
+          assertEquals(pq1.removeSmallest(), pq2.removeSmallest());
+          break;
+        case 3: // contains true
+          rdInt = rg.nextInt(curStrs.size());
+          curStr = curStrs.get(rdInt);
+          assertEquals(pq1.contains(curStr), pq2.contains(curStr));
+          break;
+        case 4: // contains likely false
+          rdInt = rg.nextInt(N * 2);
+          curStr = "s" + rdInt;
+          assertEquals(pq1.contains(curStr), pq2.contains(curStr));
+          break;
+        case 5: // changePriority
+          rdInt = rg.nextInt(curStrs.size());
+          curStr = curStrs.get(rdInt);
+          rdDb = rg.nextDouble() * R;
+          pq1.changePriority(curStr, rdDb);
+          pq2.changePriority(curStr, rdDb);
+          break;
+        default: // add
+          curStr = "s" + i;
+          rdDb = rg.nextDouble() * R;
+          curStrs.add(curStr);
+          pq1.add(curStr, rdDb);
+          pq2.add(curStr, rdDb);
+      }
+    }
+  }
 }
