df05d0c7275654166c27f7fec0dac3d14561c8dd
zuchao.wang <zuchao.wang@gmail.com>
2022-06-26 15:22:28 +0800

    proj3.lab12 ok, but no test

--- proj3/byow/lab12/HexBlock.java
+++ proj3/byow/lab12/HexBlock.java
@@ -1,62 +1,174 @@
 package byow.lab12;
 
+import java.util.Random;
+
 import byow.TileEngine.TETile;
 
 public class HexBlock {
-    // origin at top left corner 
-    private int size;
-    private int x;
-    private int y;
-    private int numTiles;
-    private int[] tileXOffsets;
-    private int[] tileYOffsets;
-    private TETile[] tiles;
-    private TETile seedTile;
+  private static final int[] tessXSteps = new int[]{1, 0, -1, -1, 0, 1};
+  private static final int[] tessYSteps = new int[]{-1, -2, -1, 1, 2, 1};
 
-    public HexBlock(int sz, TETile seed) {
-      size = sz;
-      seedTile = seed;
-      x = 0;
+  // origin at bottom left corner of bounding box
+  private int size;
+  private int x;
+  private int y;
+  private int[] tileXOffsets;
+  private int[] tileYOffsets;
+  private TETile[] tiles;
+  private TETile seedTile;
+  private Random randGen;
+  private int randColor;
+
+  public HexBlock(int sz, int xx, int yy, TETile seed, Random rg, int rc) {
+    size = sz;
+    seedTile = seed;
+    x = xx;
+    y = yy;
+    randGen = rg;
+    randColor = rc;
+    int n = numTiles(size);
+    tileXOffsets = new int[n];
+    tileYOffsets = new int[n];
+    tiles = new TETile[n];
+    calcTileOffsets();
+    calcTiles();
+  }
+
+  public void fillWorld(TETile[][] world) {
+    int n = numTiles(size);
+    for (int i=0; i<n; i++) {
+      world[x+tileXOffsets[i]][y+tileYOffsets[i]] = tiles[i];
+    }
+  }
+
+  private void calcTileOffsets() {
+    int tileIndex = 0;
+    // from bottom to top, left to right
+    for (int y = 0; y < boundingBoxHeight(size); y++) {
+      for (int x = withinHexStartX(size, y); x < withinHexEndX(size, y); x++) {
+        tileXOffsets[tileIndex] = x;
+        tileYOffsets[tileIndex] = y;
+        tileIndex++;
+      }
+    }
+  }
+
+  private void calcTiles() {
+    int n = numTiles(size);
+    for (int tileIndex=0; tileIndex<n; tileIndex++) {
+      tiles[tileIndex] = TETile.colorVariant(seedTile, randColor, randColor, randColor, randGen);
+    }
+  }
+
+  public static void tesselate(int sz, int n, int[] xs, int[] ys) {
+    valiateSize(sz);
+    valiateN(n);
+    tesselateFromZero(sz, n, xs, ys);
+    moveTesselation(sz, n, xs, ys);
+  }
+
+  private static void tesselateFromZero(int sz, int n, int[] xs, int[] ys) {
+    int oftx = interHexOffsetX(sz),
+      ofty = interHexOffsetY(sz),
+      tileIndex = 0,
+      layerIndex = 0,
+      x = 0,
       y = 0;
-      numTiles = calcNumTiles();
-      tileXOffsets = new int[numTiles];
-      tileYOffsets = new int[numTiles];
-      tiles = new TETile[numTiles];
-      fillTileOffsets();
-    }
-
-    public void fillWorld(TETile[][] world, int envX, int envY) {
-
-    }
-
-    private void fillTileOffsets() {
-      int k = 0;
-      for (int i=0; i<size; i++) {
-        for (int j=-i; j<size+i; j++) {
-          tileXOffsets[k] = j;
-          tileYOffsets[k] = i;
-          k++;
+    xs[tileIndex] = x;
+    ys[tileIndex] = y;
+    tileIndex++;
+    layerIndex++;
+    while(true) {
+      x = 0;
+      y = ofty * 2 * layerIndex;
+      for (int edgeIndex=0; edgeIndex<6; edgeIndex++) {
+        for (int i=0; i<layerIndex; i++) {
+          if (tileIndex < n) {
+            xs[tileIndex] = x;
+            ys[tileIndex] = y;
+            tileIndex++;
+            x += oftx * tessXSteps[edgeIndex];
+            y += ofty * tessYSteps[edgeIndex];
+          } else {
+            return;
+          }
         }
       }
-      for (int i=size; i<size*2; i++) {
-      }
+      layerIndex++;
     }
+  }
 
-    private int calcNumTiles() {
-      return 4 * size * size - 2 * size;
+  private static void moveTesselation(int sz, int n, int[] xs, int[] ys) {
+    int minX = Integer.MAX_VALUE, minY = Integer.MAX_VALUE;
+    for (int i=0; i<n; i++) {
+      if (xs[i] < minX) minX = xs[i];
+      if (ys[i] < minY) minY = ys[i];
     }
+    for (int i=0; i<n; i++) {
+      xs[i] -= minX;
+      ys[i] -= minY;
+    }
+  }
 
-    public static int tessOffsetX(int sz) {
-      validateSize(sz);
-      return 2 * sz - 1;
+  public static int worldWidth(int sz, int n, int[] xs) {
+    valiateSize(sz);
+    valiateN(n);
+    int maxX = Integer.MIN_VALUE;
+    for (int i=0; i<n; i++) {
+      if (xs[i] > maxX) maxX = xs[i];
     }
+    return maxX + boundingBoxWidth(sz);
+  }
 
-    public static int tessOffsetY(int sz) {
-      validateSize(sz);
-      return sz;
+  public static int worldHeight(int sz, int n, int[] ys) {
+    valiateSize(sz);
+    valiateN(n);
+    int maxY = Integer.MIN_VALUE;
+    for (int i=0; i<n; i++) {
+      if (ys[i] > maxY) maxY = ys[i];
     }
+    return maxY + boundingBoxHeight(sz);
+  }
 
-    private static void validateSize(int s) {
-      if (s < 2) throw new IllegalArgumentException();
-    }
+  private static void valiateSize(int sz) {
+    if (sz < 2) throw new IllegalArgumentException();
+  }
+
+  private static void valiateN(int n) {
+    if (n < 1) throw new IllegalArgumentException();
+  }
+
+  private static int boundingBoxWidth(int sz) {
+    return 3 * sz - 2;
+  }
+
+  private static int boundingBoxHeight(int sz) {
+    return 2 * sz;
+  }
+
+  private static int numTiles(int sz) {
+    return 4 * sz * sz - 2 * sz;
+  }
+
+  private static int interHexOffsetX(int sz) {
+    return 2 * sz - 1;
+  }
+
+  private static int interHexOffsetY(int sz) {
+    return sz;
+  }
+
+  private static int withinHexStartX(int sz, int y) {
+    if (y < sz)
+      return sz - 1 - y;
+    else
+      return y - sz;
+  }
+
+  private static int withinHexEndX(int sz, int y) {
+    if (y < sz)
+      return 2 * sz - 1 + y;
+    else
+      return 4 * sz - 2 - y;
+  }
 }
--- proj3/byow/lab12/HexWorld.java
+++ proj3/byow/lab12/HexWorld.java
@@ -12,4 +12,56 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+  private static final long SEED = 2873123;
+  private static final Random RANDOM = new Random(SEED);
+
+  public static void main(String[] args) {
+    // tesselation
+    int size = 4, 
+      n = 19;
+    int xs[] = new int[n],
+      ys[] = new int[n];
+    HexBlock.tesselate(size, n, xs, ys);
+    int worldWidth = HexBlock.worldWidth(size, n, xs),
+      worldHeight = HexBlock.worldHeight(size, n, ys);
+
+    // initialize the tile rendering engine
+    TERenderer ter = new TERenderer();
+    ter.initialize(worldWidth, worldHeight);
+
+    // initialize tiles
+    TETile[][] world = new TETile[worldWidth][worldHeight];
+    for (int x = 0; x < worldWidth; x += 1) {
+        for (int y = 0; y < worldHeight; y += 1) {
+            world[x][y] = Tileset.NOTHING;
+        }
+    }
+
+    // initialize hex-blocks
+    HexBlock[] hexs = new HexBlock[n];
+    int randColor = 32;
+    for (int i=0; i<n; i++) {
+      hexs[i] = new HexBlock(size, xs[i], ys[i], randomTile(), RANDOM, randColor);
+    }
+
+    // fill world with hex-blocks
+    for (int i=0; i<n; i++) {
+      hexs[i].fillWorld(world);
+    }
+
+    // draws the world to the screen
+    ter.renderFrame(world);
+  }
+
+  private static TETile randomTile() {
+    int tileNum = RANDOM.nextInt(5);
+    switch (tileNum) {
+        case 0: return Tileset.MOUNTAIN;
+        case 1: return Tileset.TREE;
+        case 2: return Tileset.GRASS;
+        case 3: return Tileset.FLOWER;
+        case 4: return Tileset.SAND;
+        default: return Tileset.NOTHING;
+    }
+  }
 }
